2994c1a339adf0583c1443461757e2784deb3b01
zakaryaalgeria <zz194451@gmail.com>
2023-12-15 13:59:36 +0100

    Add what we can do

--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -1,10 +1,12 @@
 import { NavBar } from "../NavBar"
 import {Hero} from '../Hero'
+import { What_we_do } from "../What_we_do"
 export const Home = () => {
   return (
     <div>
         <NavBar />
         <Hero/>
+        <What_we_do/>
     </div>
   )
 }
