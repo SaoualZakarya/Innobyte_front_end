02443b85d98e28980d9311188ee54713a9f85baa
Rayane-Boucheraine <rayaneha08@gmail.com>
2023-12-15 14:11:51 +0100

    resolve some issue

--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -1,9 +1,15 @@
 import { NavBar } from "../NavBar"
+import { Hero } from "../Hero"
+import { Features } from "../Feautures"
+import { What_we_do } from "../What_we_do"
+
 export const Home = () => {
   return (
     <div>
         <NavBar />
         <Hero/>
+        <Features />
+        <What_we_do/>
     </div>
   )
 }
