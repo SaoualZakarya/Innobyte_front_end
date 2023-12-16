92d09af3fb74837f483fa585b7b536ecf0ccfad6
zakaryaalgeria <zz194451@gmail.com>
2023-12-16 11:09:14 +0100

    Add the not found page

--- src/components/home/What_we_do.jsx
+++ src/components/home/What_we_do.jsx
@@ -1,4 +1,4 @@
-
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
 export const What_we_do = () => {
   return (
     <div  className="container py-12">
@@ -11,15 +11,13 @@ export const What_we_do = () => {
         <div className="flex flex-wrap gap-14 lg:gap-0 lg:justify-between justify-center">
             
             <div className="w-[300px] flex flex-col items-center   ">
-                <div className="w-10 bg-[#FED369] h-10 rounded-2xl flex justify-center items-center">
-                    <img src="/icons/graph.png" className="w-6 h-6" alt="g" />
+                <div className="w-10 bg-[#3ADAD9] h-10 rounded-2xl flex justify-center items-center">
+                    <IoChatboxEllipsesOutline className="w-6 h-6 text-white "/>
                 </div>
                 <div className="p-5 rounded-xl shadow-lg">
-                    <h2 className="text-[#2B3377] text-[22px] py-2 text-center"> Market Analysis</h2>
-                    <p>
-                        A market analysis is a quantitative and 
-                        qualitative assessment of a market.
-                        It looks into the size of the market.
+                    <h2 className="text-[#2B3377] text-[22px] py-2 text-center"> Efficient Communication</h2>
+                    <p className="text-center">
+                        By providing real-time messaging, video conferencing, and seamless file sharing, fostering an efficient communication environment
                     </p>
                 </div>
             </div>
@@ -28,11 +26,9 @@ export const What_we_do = () => {
                     <img src="/icons/graph.png" className="w-6 h-6" alt="g" />
                 </div>
                 <div className="p-5 rounded-xl shadow-lg">
-                    <h2 className="text-[#2B3377] text-[22px] py-2 text-center"> Market Analysis</h2>
-                    <p>
-                        A market analysis is a quantitative and 
-                        qualitative assessment of a market.
-                        It looks into the size of the market.
+                    <h2 className="text-[#2B3377] text-[22px] py-2 text-center">flexibilty</h2>
+                    <p className="text-center">
+                    by breaking barriers, allowing teams to collaborate anytime, anywhere, promoting quick decision-making and idea exchange.
                     </p>
                 </div>
             </div>
@@ -41,11 +37,9 @@ export const What_we_do = () => {
                     <img src="/icons/graph.png" className="w-6 h-6" alt="g" />
                 </div>
                 <div className="p-5 rounded-xl shadow-lg">
-                    <h2 className="text-[#2B3377] text-[22px] py-2 text-center"> Market Analysis</h2>
-                    <p>
-                        A market analysis is a quantitative and 
-                        qualitative assessment of a market.
-                        It looks into the size of the market.
+                    <h2 className="text-[#2B3377] text-[22px] py-2 text-center"> enhanced productivity</h2>
+                    <p className="text-center">
+                    through our integrated suggestion system that studies chat based concerns and works on resolving them
                     </p>
                 </div>
             </div>
